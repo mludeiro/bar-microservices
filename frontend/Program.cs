832f9622960e7b0a968d55822852b0618ec0ec37
Martin Ludeiro <martin.ludeiro@gmail.com>
2024-02-05 21:29:37 -0300

    CORS for local development

--- frontend/Program.cs
+++ frontend/Program.cs
@@ -6,6 +6,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+var addr = builder.HostEnvironment.IsEnvironment("Development") ? "http://localhost:80/" : builder.HostEnvironment.BaseAddress;
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(addr) });
 
 await builder.Build().RunAsync();
